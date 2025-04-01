c7414c1721cec8d94a1c26ec2f1f29d265c2cd22
brilliantchikanya <brilliant.chikanya@gmail.com>
2025-04-01 08:40:34 +0200

    added add button listener

--- src/main/java/com/bullet/sqlite3/controller/EmployeeController.java
+++ src/main/java/com/bullet/sqlite3/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.bullet.sqlite3.model.Employee;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -25,26 +22,21 @@ public class EmployeeController implements Initializable {
     public TableView<Employee> tbl_employees;
     public TextField tf_firstname;
     public TextField tf_lastname;
+    public TextField tf_employeeNumber;
     ObservableList<Gender> gender = FXCollections.observableArrayList(MALE, FEMALE, UNKNOWN);
     ObservableList<Employee> employees = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        //cb_gender.setItems(FXCollections.observableList(gender));
-        cb_gender.getItems().addAll(gender);
-        //addListeners();
-        btn_add_employee.setOnAction(event -> System.out.println("bullet"));
-        System.out.println("Starting");
-
+        cb_gender.setItems(gender);
+        addListeners();
     }
 
-//    private void addListeners() {
-//        //btn_add_employee.setOnAction(event -> System.out.println("bullet"));
-//
-//    }
-//
-//    private void onAdd() {
-//        System.out.println("Add button clicked");
-//        tf_firstname.setText("Hie");
-//    }
+    private void addListeners() {
+        btn_add_employee.setOnAction(event -> onAdd());
+    }
+
+    private void onAdd() {
+        System.out.println("Add");
+    }
 }
