2d16b3d9e9eb200c29d45ec7e3ebaba1cea10cbe
brilliantchikanya <brilliant.chikanya@gmail.com>
2025-04-03 10:35:53 +0200

    added payment type to ui

--- src/main/java/com/bullet/sqlite3/controller/EmployeePaymentDetailsController.java
+++ src/main/java/com/bullet/sqlite3/controller/EmployeePaymentDetailsController.java
@@ -1,5 +1,8 @@
 package com.bullet.sqlite3.controller;
 
+import com.bullet.employee.strategy.PaymentType;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
@@ -8,6 +11,8 @@ import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import static com.bullet.employee.strategy.PaymentType.*;
 
 public class EmployeePaymentDetailsController implements Initializable {
     public TextField tf_employee_number;
@@ -29,8 +34,23 @@ public class EmployeePaymentDetailsController implements Initializable {
     public TableColumn col_department;
     public TableColumn col_payment_type;
 
+    /********    OBSERVABLE LISTS  ****/
+    private final ObservableList paymentType = FXCollections.observableArrayList(HOURLY, DAILY, MONTHLY, CONTRACT);
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        /******     COMBO BOXES   */
+        cb_payment_type.setItems(paymentType);
+
+
+        /******     TABLE VIEW   */
+
+        /******     BUTTON EVENTS   */
+
+        /******     TABLE COLUMNS   */
+
+        /******     COMBO BOX   */
+
 
     }
 }
