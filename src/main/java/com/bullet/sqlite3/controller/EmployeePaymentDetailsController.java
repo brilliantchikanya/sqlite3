fb70afd21443a5edfaacb32489f6b6d2bc1560ad
brilliantchikanya <brilliant.chikanya@gmail.com>
2025-04-03 10:26:04 +0200

    added paymentDetails UI

--- src/main/java/com/bullet/sqlite3/controller/EmployeePaymentDetailsController.java
+++ src/main/java/com/bullet/sqlite3/controller/EmployeePaymentDetailsController.java
@@ -1,11 +1,34 @@
 package com.bullet.sqlite3.controller;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class EmployeePaymentDetailsController implements Initializable {
+    public TextField tf_employee_number;
+    public ComboBox cb_payment_type;
+    public ComboBox cb_pay_type_strategy;
+    public TextField tf_department;
+    public TextField tf_job_title;
+    public TextField tf_hourly_rate;
+    public TextField tf_hours_worked;
+    public TextField tf_standard_hours;
+    public TextField tf_daily_rate;
+    public TextField tf_days_worked;
+    public TextField tf_standard_days;
+    public TextField tf_monthly_salary;
+    public DatePicker dp_date_joined;
+    public TableColumn col_employee_number;
+    public TableColumn col_firstname;
+    public TableColumn col_lastname;
+    public TableColumn col_department;
+    public TableColumn col_payment_type;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
